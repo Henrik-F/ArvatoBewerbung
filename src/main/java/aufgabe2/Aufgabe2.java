fe28b60aa5701a5f11bd513e5601cec65bea91ab
Henrik-F <henrikfengler@gmail.com>
2019-11-20 21:20:34 +0100

    Aufgabe2 is fully functional

--- src/main/java/aufgabe2/Aufgabe2.java
+++ src/main/java/aufgabe2/Aufgabe2.java
@@ -2,7 +2,85 @@ package aufgabe2;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Aufgabe2 {
+
+    String getMetaData(String jsonInput){
+        ProductData[] productDates = convertToObjects(jsonInput);
+        String mostExpensiveProduct = getMostExpensiveProduct(productDates);
+        String cheapestProduct = getCheapestProduct(productDates);
+        String mostPopularProduct = getMostPopularProduct(productDates);
+        String[] germanProducts = getGermanProducts(productDates);
+        String[] chineseProducts = getChineseProducts(productDates);
+        boolean containsFragileProducts = getContainsFragileProducts(productDates);
+
+        MetaData metaData = new MetaData(mostExpensiveProduct, cheapestProduct, mostPopularProduct,
+                germanProducts, chineseProducts, containsFragileProducts);
+
+        Gson gson = new Gson();
+        return gson.toJson(metaData);
+    }
+
+    String getMostExpensiveProduct(ProductData[] productDates) {
+        ProductData mostExpensiveProduct = productDates[0];
+        for (ProductData product : productDates) {
+            if(product.getPrice() > mostExpensiveProduct.getPrice()){
+                mostExpensiveProduct = product;
+            }
+        }
+        return mostExpensiveProduct.getName();
+    }
+
+    String getCheapestProduct(ProductData[] productDates) {
+        ProductData cheapestProduct = productDates[0];
+        for (ProductData product : productDates) {
+            if(product.getPrice() < cheapestProduct.getPrice()){
+                cheapestProduct = product;
+            }
+        }
+        return cheapestProduct.getName();
+    }
+
+    String getMostPopularProduct(ProductData[] productDates) {
+        ProductData mostPopularProduct = productDates[0];
+        for (ProductData product : productDates) {
+            if(product.getTimesPurchased() > mostPopularProduct.getTimesPurchased()){
+                mostPopularProduct = product;
+            }
+        }
+        return mostPopularProduct.getName();
+    }
+
+    String[] getGermanProducts(ProductData[] productDates) {
+        List<String> germanProductsLlist = new ArrayList<>();
+        for (ProductData product : productDates) {
+            if(product.getCountryofOrigin().equals("DE")){
+                germanProductsLlist.add(product.getName());
+            }
+        }
+        return germanProductsLlist.toArray(new String[0]);
+    }
+
+    String[] getChineseProducts(ProductData[] productDates) {
+        List<String> chineseProducts = new ArrayList<>();
+        for (ProductData product : productDates) {
+            if(product.getCountryofOrigin().equals("CN")){
+                chineseProducts.add(product.getName());
+            }
+        }
+        return chineseProducts.toArray(new String[0]);
+    }
+
+    boolean getContainsFragileProducts(ProductData[] productDates) {
+        for (ProductData product : productDates) {
+            if(product.getIsFragile()){
+                return true;
+            }
+        }
+        return false;
+    }
 
     ProductData[] convertToObjects(String jsonInput) {
         Gson gson = new Gson();
@@ -13,8 +91,7 @@ class Aufgabe2 {
     String getProductName(String jsonInput) {
         Gson gson = new Gson();
         ProductData obj = gson.fromJson(jsonInput, ProductData.class);
-        String productName = obj.getName();
-        return productName;
+        return obj.getName();
     }
 
     double getProductPrice() {
@@ -93,5 +170,25 @@ class ProductData {
         this.price = price;
         this.isFragile = isFragile;
         this.timesPurchased = timesPurchased;
+    }
+}
+
+class MetaData{
+    String mostExpensiveProduct;
+    String cheapestProduct;
+    String mostPopularProduct;
+    String[] germanProducts;
+    String[] chineseProducts;
+    boolean containsFragileProducts;
+
+    MetaData(String mostExpensiveProduct, String cheapestProduct, String mostPopularProduct,
+            String[] germanProducts, String[] chineseProducts,
+             boolean containsFragileProducts){
+        this.mostExpensiveProduct = mostExpensiveProduct;
+        this.cheapestProduct = cheapestProduct;
+        this.mostPopularProduct = mostPopularProduct;
+        this.germanProducts = germanProducts;
+        this.chineseProducts = chineseProducts;
+        this.containsFragileProducts = containsFragileProducts;
     }
 }
--- src/test/java/aufgabe2/Aufgabe2Test.java
+++ src/test/java/aufgabe2/Aufgabe2Test.java
@@ -45,5 +45,110 @@ public class Aufgabe2Test {
         assertArrayEquals(new ProductData[]{obj1, obj2}, new Aufgabe2().convertToObjects(jsonInput));
     }
 
+    @Test
+    public void getMostExpensiveProduct_TwoProductsOnlyDifferByNameAndPrice() {
+        ProductData[] productDates = {new ProductData("product1", "DE", 5.50, false, 0),
+        new ProductData("product2", "DE", 4.50, false, 0)};
+
+        assertEquals("product1", new Aufgabe2().getMostExpensiveProduct(productDates));
+    }
+
+
+    @Test
+    public void getCheapestProduct_TwoProductsOnlyDifferByNameAndPrice() {
+        ProductData[] productDates = {new ProductData("product1", "DE", 5.50, false, 0),
+        new ProductData("product2", "DE", 4.50, false, 0)};
+
+        assertEquals("product2", new Aufgabe2().getCheapestProduct(productDates));
+    }
+
+    @Test
+    public void getMostPopularProduct_TwoProductsOnlyDifferByNameAndTimesPurchased() {
+        ProductData[] productDates = {new ProductData("product1", "DE", 5.50, false, 10),
+        new ProductData("product2", "DE", 5.50, false, 5)};
+
+        assertEquals("product1", new Aufgabe2().getMostPopularProduct(productDates));
+    }
+
+    @Test
+    public void getGermanProducts_NoGermanProductsUsed() {
+        ProductData[] productDates = {new ProductData("product1", "AD", 5.50, false, 0),
+        new ProductData("product2", "AE", 5.50, false, 0)};
+
+       assertArrayEquals(new String[]{}, new Aufgabe2().getGermanProducts(productDates));
+    }
+
+    @Test
+    public void getGermanProducts_TwoOutOfThreeAreGerman() {
+        ProductData[] productDates = {new ProductData("product1", "DE", 5.50, false, 0),
+        new ProductData("product2", "AE", 5.50, false, 0),
+                new ProductData("product3", "DE", 10.99, false, 0)};
+
+       assertArrayEquals(new String[]{"product1", "product3"}, new Aufgabe2().getGermanProducts(productDates));
+    }
+
+    @Test
+    public void getChineseProducts_NoChineseProductsUsed() {
+        ProductData[] productDates = {new ProductData("product1", "DE", 5.50, false, 0),
+        new ProductData("product2", "AE", 5.50, false, 0),
+                new ProductData("product3", "DE", 10.99, false, 0)};
+
+       assertArrayEquals(new String[]{}, new Aufgabe2().getChineseProducts(productDates));
+    }
+
+    @Test
+    public void getChineseProducts_TwoOutOfThreeAreChinese() {
+        ProductData[] productDates = {new ProductData("product1", "DE", 5.50, false, 0),
+        new ProductData("product2", "CN", 5.50, false, 0),
+                new ProductData("product3", "CN", 10.99, false, 0)};
+
+       assertArrayEquals(new String[]{"product2", "product3"}, new Aufgabe2().getChineseProducts(productDates));
+    }
+
+    @Test
+    public void getContainsFragileProducts_ShouldBeFalse() {
+        ProductData[] productDates = {new ProductData("product1", "DE", 5.50, false, 0),
+        new ProductData("product2", "CN", 5.50, false, 0),
+                new ProductData("product3", "CN", 10.99, false, 0)};
+
+       assertFalse(new Aufgabe2().getContainsFragileProducts(productDates));
+    }
+
+    @Test
+    public void getContainsFragileProducts_ShouldBeTrue() {
+        ProductData[] productDates = {new ProductData("product1", "DE", 5.50, false, 0),
+        new ProductData("product2", "CN", 5.50, true, 0),
+                new ProductData("product3", "CN", 10.99, false, 0)};
+
+       assertTrue(new Aufgabe2().getContainsFragileProducts(productDates));
+    }
+
+    @Test
+    public void getMetaData_returnsAllDataInJsonReadableFormat() {
+        String jsonInput = "[{\"name\":\"product1\"," +
+                "\"countryofOrigin\":\"DE\"," +
+                "\"price\":5.50," +
+                "\"isFragile\":false," +
+                "\"timesPurchased\":10}," +
+                "{\"name\":\"product2\"," +
+                "\"countryofOrigin\":\"DE\"," +
+                "\"price\":8.00," +
+                "\"isFragile\":false," +
+                "\"timesPurchased\":100}," +
+                "{\"name\":\"product3\"," +
+                "\"countryofOrigin\":\"CN\"," +
+                "\"price\":7.50," +
+                "\"isFragile\":true," +
+                "\"timesPurchased\":20}]";
+        String expectedOutput = "{\"mostExpensiveProduct\":\"product2\"," +
+                "\"cheapestProduct\":\"product1\"," +
+                "\"mostPopularProduct\":\"product2\"," +
+                "\"germanProducts\":[\"product1\",\"product2\"]," +
+                "\"chineseProducts\":[\"product3\"]," +
+                "\"containsFragileProducts\":true}";
+        assertEquals(expectedOutput, new Aufgabe2().getMetaData(jsonInput));
+    }
+
+
 
 }
