79394ca2eb1f6a695e56fab563192d0c392c69a2
Henrik-F <henrikfengler@gmail.com>
2019-11-20 20:01:25 +0100

    Made some little Progress with Aufgabe 2

--- src/main/java/aufgabe2/Aufgabe2.java
+++ src/main/java/aufgabe2/Aufgabe2.java
@@ -3,26 +3,80 @@ package aufgabe2;
 import com.google.gson.Gson;
 
 class Aufgabe2 {
+
+    ProductData[] convertToObjects(String jsonInput) {
+        Gson gson = new Gson();
+        ProductData[] objArray = gson.fromJson(jsonInput, ProductData[].class);
+
+        for (ProductData obj : objArray) {
+            System.out.println(obj.getName() + "\n" + obj.getCountryofOrigin() + "\n" + obj.getPrice() + "\n"
+                    + obj.getIsFragile() + "\n" + obj.getTimesPurchased());
+        }
+        return objArray;
+    }
+
     String getProductName(String jsonInput) {
         Gson gson = new Gson();
-        ProductValues obj = gson.fromJson(jsonInput, ProductValues.class);
+        ProductData obj = gson.fromJson(jsonInput, ProductData.class);
         String productName = obj.getName();
         System.out.println(productName);
         return productName;
     }
+
+    double getProductPrice() {
+        return 0.0;
+    }
+
+    int getTimesPurchased() {
+        return 0;
+    }
+
+    boolean isGerman() {
+/*        ProductValues thisProduct;
+        if(thisProduct.getCountryofOrigin().equals("DE")){
+            return true;
+        }*/
+        return false;
+    }
+
+    boolean isChinese() {
+        return false;
+    }
+
+    boolean getIsFragile() {
+        return false;
+    }
+
 }
-class ProductValues{
+
+class ProductData {
     private String name;
     private String countryofOrigin;
     private double price;
     private boolean isFragile;
     private int timesPurchased;
 
-    String getName(){
+    String getName() {
         return name;
     }
 
-    ProductValues(String name, String countryofOrigin, double price, boolean isFragile, int timesPurchased){
+    public String getCountryofOrigin() {
+        return countryofOrigin;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean getIsFragile() {
+        return isFragile;
+    }
+
+    public int getTimesPurchased() {
+        return timesPurchased;
+    }
+
+    ProductData(String name, String countryofOrigin, double price, boolean isFragile, int timesPurchased) {
         this.name = name;
         this.countryofOrigin = countryofOrigin;
         this.price = price;
--- src/test/java/aufgabe2/Aufgabe2Test.java
+++ src/test/java/aufgabe2/Aufgabe2Test.java
@@ -25,5 +25,32 @@ public class Aufgabe2Test {
         assertEquals("product1", new Aufgabe2().getProductName(jsonInput));
     }
 
+    @Test
+    public void convertingjsonInputToObjectArray_twoObjectsNotEmpty() {
+        String jsonInput = "[{\"name\":\"asdf\"," +
+                "\"countryofOrigin\":\"DE\"," +
+                "\"price\":5.50," +
+                "\"isFragile\":false," +
+                "\"timesPurchased\":10}," +
+                "{\"name\":\"product1\"," +
+                "\"countryofOrigin\":\"DE\"," +
+                "\"price\":7.50," +
+                "\"isFragile\":true," +
+                "\"timesPurchased\":20}]";
+        ProductData obj1 = new ProductData("asdf", "DE", 5.50, false,
+                10);
+        ProductData obj2 = new ProductData("product1", "DE", 7.50, true,
+                20);
+
+        ProductData[] objArray = new Aufgabe2().convertToObjects(jsonInput);
+        for (ProductData obj : objArray) {
+            System.out.println(obj.getName() + "\n" + obj.getCountryofOrigin() + "\n" + obj.getPrice() + "\n"
+                    + obj.getIsFragile() + "\n" + obj.getTimesPurchased());
+        }
+        // assertArrayEquals(new ProductData[]{obj1, obj2}, new Aufgabe2().convertToObjects(jsonInput));
+        assertEquals(obj1, new Aufgabe2().convertToObjects(jsonInput)[0]);
+
+    }
+
 
 }
