f909b6b65ed37a3e8d892b226e17e5af67b4b707
Henrik-F <henrikfengler@gmail.com>
2019-11-21 22:01:11 +0100

    Refactored Aufgabe3

--- src/main/java/aufgabe3/Aufgabe3.java
+++ src/main/java/aufgabe3/Aufgabe3.java
@@ -2,57 +2,48 @@ package aufgabe3;
 
 import java.util.*;
 
-public class Aufgabe3 {
+class Aufgabe3 {
 
-    List<Flea[]> nOverKCombinations = new ArrayList<>();
+    List<Flea[]> combinationsOfLengthK = new ArrayList<>();
 
     int getOptimalValue(float money, List<Flea> fleas) {
-        List<Flea[]> allCombinations = getAllCombinations(fleas);
         int res = 0;
-        for (Flea[] fleaArr : allCombinations) {
-            int valueOfThisArray = 0;
-            float priceOfThisArray = 0;
-            for (Flea flea : fleaArr) {
-                valueOfThisArray += flea.getRating();
-                priceOfThisArray += flea.getPrice();
+        List<Flea[]> allCombinations = getAllCombinations(fleas);
+        for (Flea[] possibleCombination : allCombinations) {
+            int valueOfThisCombination = 0;
+            float priceOfThisCombination = 0;
+            for (Flea flea : possibleCombination) {
+                valueOfThisCombination += flea.getRating();
+                priceOfThisCombination += flea.getPrice();
             }
-            if(priceOfThisArray <= money && valueOfThisArray > res){
-                res = valueOfThisArray;
+            if (priceOfThisCombination <= money && valueOfThisCombination > res) {
+                res = valueOfThisCombination;
             }
         }
         return res;
     }
 
     List<Flea[]> getAllCombinations(List<Flea> fleas) {
-        //TODO get all combinations n over n (1) and n over k (for 0 < k < n)
         List<Flea[]> allCombinations = new ArrayList<>();
 
         for (int i = 1; i <= fleas.size(); i++) {
-            combinationsNOverK(fleas, i, 0, new Flea[i]);
-            allCombinations.addAll(nOverKCombinations);
-            nOverKCombinations.clear();
+            getAllCombinationsWithKElements(fleas, i, 0, new Flea[i]);
+            allCombinations.addAll(combinationsOfLengthK);
+            combinationsOfLengthK.clear();
         }
-        for (Flea[] fleaArr : allCombinations) {
-            System.out.println("Combination:");
-            for (Flea flea : fleaArr) {
-                System.out.println(flea.getName());
-            }
-        }
-
         return allCombinations;
     }
 
-    void combinationsNOverK(List<Flea> fleas, int k, int start, Flea[] intermediateResult) {
+    void getAllCombinationsWithKElements(List<Flea> fleas, int k, int start, Flea[] intermediateResult) {
         if (k == 0) {
             Flea[] oneCombination = new Flea[intermediateResult.length];
             System.arraycopy(intermediateResult, 0, oneCombination, 0, intermediateResult.length);
-            nOverKCombinations.add(oneCombination);
+            combinationsOfLengthK.add(oneCombination);
             return;
         }
         for (int i = start; i <= fleas.size() - k; i++) {
             intermediateResult[intermediateResult.length - k] = fleas.get(i);
-            combinationsNOverK(fleas, k - 1, i + 1, intermediateResult);
+            getAllCombinationsWithKElements(fleas, k - 1, i + 1, intermediateResult);
         }
     }
-
 }
--- src/test/java/aufgabe3/Aufgabe3Test.java
+++ src/test/java/aufgabe3/Aufgabe3Test.java
@@ -28,7 +28,26 @@ public class Aufgabe3Test {
     }
 
     @Test
-    public void blablaDeleteLater() {
+    public void getAllCombinationsWithKElements_OneElementOutOfThree() {
+        Flea flea1 = new Flea("flea1", 5, 10);
+        Flea flea2 =  new Flea("flea2", 5, 9);
+        Flea flea3 = new Flea("flea3", 5, 10);
+
+        List<Flea> fleas = Arrays.asList(flea1, flea2, flea3);
+
+        List<Flea[]> expected = new ArrayList<>();
+        expected.add(new Flea[]{flea1});
+        expected.add(new Flea[]{flea2});
+        expected.add(new Flea[]{flea3});
+
+        Aufgabe3 aufgabe3 = new Aufgabe3();
+
+        aufgabe3.getAllCombinationsWithKElements(fleas, 1, 0, new Flea[1]);
+
+        assertArrayEquals(expected.toArray(), aufgabe3.combinationsOfLengthK.toArray());
+    }
+    @Test
+    public void getAllCombinationsWithKElements_ThreeElementsOutOfFour() {
         Flea flea1 = new Flea("flea1", 5, 10);
         Flea flea2 =  new Flea("flea2", 5, 9);
         Flea flea3 = new Flea("flea3", 5, 10);
@@ -44,23 +63,30 @@ public class Aufgabe3Test {
 
         Aufgabe3 aufgabe3 = new Aufgabe3();
 
-        aufgabe3.combinationsNOverK(fleas, 3, 0, new Flea[3]);
+        aufgabe3.getAllCombinationsWithKElements(fleas, 3, 0, new Flea[3]);
 
-        for (Flea[] fleaArr : aufgabe3.nOverKCombinations) {
-            System.out.println("Array:");
-            for (Flea flea : fleaArr) {
-                System.out.println(flea.getName());
-
-            }
-        }
-
-        assertArrayEquals(expected.toArray(), aufgabe3.nOverKCombinations.toArray());
+        assertArrayEquals(expected.toArray(), aufgabe3.combinationsOfLengthK.toArray());
     }
 
     @Test
-    public void createAllPossibleCombinationsOfFleasRegardlessMoney() {
+    public void getAllCombinations_TwoElements() {
         Flea flea1 = new Flea("flea1", 5, 10);
-        Flea flea2 =  new Flea("flea1", 5, 9);
+        Flea flea2 = new Flea("flea2", 5, 9);
+
+        List<Flea> fleas = Arrays.asList(flea1, flea2);
+
+        List<Flea[]> expected = new ArrayList<>();
+        expected.add(new Flea[]{flea1});
+        expected.add(new Flea[]{flea2});
+        expected.add(new Flea[]{flea1, flea2});
+
+        assertArrayEquals(expected.toArray(), new Aufgabe3().getAllCombinations(fleas).toArray());
+    }
+
+    @Test
+    public void getAllCombinations_ThreeElements() {
+        Flea flea1 = new Flea("flea1", 5, 10);
+        Flea flea2 = new Flea("flea2", 5, 9);
         Flea flea3 = new Flea("flea3", 5, 10);
 
         List<Flea> fleas = Arrays.asList(flea1, flea2, flea3);
@@ -86,7 +112,7 @@ public class Aufgabe3Test {
     }
 
     @Test
-    public void getOptimalValue_NoMoney_ShoudReturnZero() {
+    public void getOptimalValue_NoMoney_ShouldReturnZero() {
         Flea flea1 = new Flea("flea1", 1, 3);
         Flea flea2 = new Flea("flea1", 5.25f, 5);
         List<Flea> fleas = Arrays.asList(flea1, flea2);
@@ -94,5 +120,18 @@ public class Aufgabe3Test {
         assertEquals(0, new Aufgabe3().getOptimalValue(0, fleas));
     }
 
+    @Test
+    public void getOptimalValue_ManyPossibilities() {
+        Flea flea1 = new Flea("flea1", 1, 3);
+        Flea flea2 = new Flea("flea2", 5.50f, 5);
+        Flea flea3 = new Flea("flea3", 5.99f, 7);
+        Flea flea4 = new Flea("flea4", 5.25f, 6);
+        Flea flea5 = new Flea("flea5", 8, 9);
+        Flea flea6 = new Flea("flea6", 4, 5);
+        Flea flea7 = new Flea("flea7", 7.80f, 8);
 
+        List<Flea> fleas = Arrays.asList(flea1, flea2, flea3, flea4, flea5, flea6, flea7);
+
+        assertEquals(24, new Aufgabe3().getOptimalValue(20, fleas));
+    }
 }
