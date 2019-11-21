07efa88b63d0352558ad6ab6c23e57f678ed6acd
Henrik-F <henrikfengler@gmail.com>
2019-11-21 14:25:48 +0100

    Started with Aufgabe 3 some work to do with the possible combinations

---
+++ src/main/java/aufgabe3/Aufgabe3.java
@@ -0,0 +1,110 @@
+package aufgabe3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Aufgabe3 {
+
+    /*    public static int getOptimalValue(float money, List<Flea> fleas){
+            int res = 0;
+            for (Flea flea : fleas) {
+                if(money >= flea.getPrice()){
+                    res += flea.getRating();
+                    money -= flea.getPrice();
+                }
+            }
+            return res;
+        }*/
+    public static int getOptimalValue(float money, List<Flea> fleas) {
+        Map fleasByValuePerPrice = new HashMap();
+        for (Flea flea : fleas) {
+            double valueByPrice = flea.getRating() / flea.getPrice();
+            fleasByValuePerPrice.put(flea, valueByPrice);
+        }
+        int res = 0;
+        for (Flea flea : fleas) {
+            if (money >= flea.getPrice()) {
+                res += flea.getRating();
+                money -= flea.getPrice();
+            }
+        }
+        return res;
+    }
+
+    public List<Flea[]> getAllCombinations(List<Flea> fleas) {
+        //TODO get all combinations n over n (1) and n over k (for 0 < k < n)
+        List<Flea[]> allCombinations = new ArrayList<>();
+        //add All Single fleas
+        for (int i = 1; i <= fleas.size(); i++) {
+            getCombinationsNOverK(fleas, i);
+        }
+        for (Flea[] fleaArr : allCombinations) {
+            System.out.println("Combination:");
+            for (Flea flea : fleaArr) {
+                System.out.println(flea.getName());
+            }
+        }
+
+        return allCombinations;
+    }
+
+    List<Flea[]> getCombinationsNOverK(List<Flea> fleas, int k) {
+        List<Flea[]> combinations = new ArrayList<>();
+
+        if (fleas.size() == k) {
+            combinations.add(fleas.toArray(new Flea[0]));
+            return combinations;
+        }
+        if (k == 1) {
+            for (Flea flea : fleas) {
+                combinations.add(new Flea[]{flea});
+            }
+            return combinations;
+        }
+        if (k == 2) {
+            for (int i = 0; i < fleas.size(); i++) {
+                for (int j = 0; j < fleas.size(); j++) {
+                    if (j > i) {
+                        combinations.add(new Flea[]{fleas.get(i), fleas.get(j)});
+                    }
+                }
+            }
+        }
+        if (k == 3) {
+            for (int i = 0; i < fleas.size(); i++) {
+                for (int j = 0; j < fleas.size(); j++) {
+                    for (int l = 0; l < fleas.size(); l++) {
+                        if (j > i && l > j) {
+                            combinations.add(new Flea[]{fleas.get(i), fleas.get(j), fleas.get(l)});
+                        }
+                    }
+                }
+            }
+        }
+
+        return combinations;
+    }
+
+    int numberOfPossibleCombinations(int numberOfFleas) {
+        int possibleCombinations = 0;
+        for (int i = 1; i <= numberOfFleas; i++) {
+            possibleCombinations += binomialCoefficient(numberOfFleas, i);
+        }
+        return possibleCombinations;
+    }
+
+    static long binomialCoefficient(int n, int k) {
+        return faculty(n) / (faculty(k) * faculty(n - k));
+    }
+
+    static long faculty(long n) {
+        long result = 1L;
+        for (long i = 1L; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/aufgabe3/Flea.java
@@ -0,0 +1,56 @@
+package aufgabe3;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Flea {
+    String name;
+    float price;
+    int rating;
+    public Flea(){}
+    public Flea(final String name, final float price, final int rating){
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+
+    @Override
+    public boolean equals(Object object){
+        if(this == object) return true;
+        if(object == null || getClass() != object.getClass()) return false;
+        if(!super.equals(object)) return false;
+        Flea flea = (Flea) object;
+        return java.util.Objects.equals(name, flea.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(super.hashCode(), name);
+    }
+}
